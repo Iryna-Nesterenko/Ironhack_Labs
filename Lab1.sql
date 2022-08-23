e6d2b5cf3dd1d87f918dc0b4ccb51331bcbaf76c
Iryna-Nesterenko <109985847+Iryna-Nesterenko@users.noreply.github.com>
2022-08-23 09:57:57 +0200

    Update Lab1.sql

--- Lab1.sql
+++ Lab1.sql
@@ -6,7 +6,8 @@ select * from film;
 select title from film;
 
 #select one column from a table and alias it. Get unique list of film languages under the alias language:
-select language_id AS language FROM film;
+select * from language;
+select name AS language FROM sakila.language;
 
 # Find out how many stores does the company have?
 select count(*) from store; 
